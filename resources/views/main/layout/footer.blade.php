b8f68e3deed234ef51f96eb2d48f4c61091debe1
nazarfadil <nazarfadilah0192728@gmail.com>
2026-07-09 22:02:10 +0800

    feat: implement dynamic footer components with database-driven copyright and branding info

--- resources/views/main/layout/footer.blade.php
+++ resources/views/main/layout/footer.blade.php
@@ -1,4 +1,8 @@
+@php
+    $copyright = \App\Models\Tentang::where('key', 'copyright')->first()?->value ?? '© 2026 Asrama Haji Emberkasi Landasan Ulin. Hak Cipta Dilindungi Undang-Undang.';
+    $naungan = \App\Models\Tentang::where('key', 'naungan')->first()?->value ?? 'Dibawah naungan <strong>Kementerian Haji RI</strong> - Kanwil Kalimantan Selatan';
+@endphp
 <p style="margin: 0;">
-    &copy;2026 Asrama Haji Emberkasi Landasan Ulin. Hak Cipta Dilindungi Undang-Undang.<br>
-    Dibawah naungan <strong>Kementrian Haji RI</strong> - Kanwil Kalimantan Selatan
+    {!! $copyright !!}<br>
+    {!! $naungan !!}
 </p>
